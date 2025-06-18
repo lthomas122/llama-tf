54f6f59f1583b796762821ea9d173c8d1042f19c
lthomas122 <liamdanielthomas@gmail.com>
2025-06-18 13:23:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,6 @@ terraform {
 }
 
 provider "aws" {
-  shared_config_files      = ["~/.aws/config"]
-  shared_credentials_files = ["~/.aws/credentials"]
   region                   = "eu-west-2"
 }
 
